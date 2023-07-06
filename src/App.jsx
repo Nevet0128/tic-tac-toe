8ba1139d8b02ece80e25616fe4930a6f1fcdbd3e
Carlos Medina <keystrike95@gmail.com>
2023-07-05 19:21:58 -0600

    inicializando el tablero con el componente Square

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,30 @@ const TURNS = {
   O: "o",
 };
 
-const board = Array(9).fill(null); //array  de 9 posiciones llenado con null
+//Creación del nuevo componente: Cuadrado que conforma el gato
+//parámetros:
+//'children' para mostrar la X / O
+//'updateSquare': para actualizar el tablero
+//'index' para saber la posición del cuadro donde se hizo click
+const Square = ({ children, updateSquare, index }) => {
+  return <div className="square">{children}</div>;
+};
 
 function App() {
+  //necesario tenerlo dentro del componente para guardar los estados del tablero
+  const board = Array(9).fill(null); //array  de 9 posiciones llenado con null
+
   return (
     <main className="board">
       <h1>Juego del gato</h1>
       <section className="game">
-        {/*Se encierra con llaves al principio porque se 
-        está evaluando las variables, siempre tiene que ser así */}
         {board.map((_, index) => {
           return (
-            <div key={index}>
-              <span>{index}</span>
-            </div>
+            <Square
+              key={index} //identificador único que se renderiza de una lista
+              //CON LA EXTENSIÓN DE REACT EN CHROME SE PUEDEN VER LOS COMPONENTES CON SU KEY
+              index={index} // para pasar por parámetro a Square
+            ></Square>
           );
         })}
       </section>
