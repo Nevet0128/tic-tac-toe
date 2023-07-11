8c2592a83bb480ef6d69dcd6089121d9fb1f4905
Carlos Medina <keystrike95@gmail.com>
2023-07-10 20:56:26 -0600

    Creación de los estados y de la estructura básica del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const TURNS = {
@@ -7,30 +8,61 @@ const TURNS = {
 
 //Creación del nuevo componente: Cuadrado que conforma el gato
 //parámetros:
-//'children' para mostrar la X / O
-//'updateSquare': para actualizar el tablero
-//'index' para saber la posición del cuadro donde se hizo click
-const Square = ({ children, updateSquare, index }) => {
-  return <div className="square">{children}</div>;
+//'children': para mostrar la X / O
+//'updateBoard': para actualizar el tablero ---CAMBIADO---
+//'index': para saber la posición del cuadro donde se hizo click
+const Square = ({ children, updateBoard, index }) => {
+  const handleClick = () => {
+    updateBoard();
+  };
+
+  return (
+    <div className="square" onClick={handleClick}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
-  //necesario tenerlo dentro del componente para guardar los estados del tablero
-  const board = Array(9).fill(null); //array  de 9 posiciones llenado con null
+  //Para poder guardar el avance del juego se deben de usar un estado
+  const [board, setBoard] = useState(Array(9).fill(null)); //El estado inicial se da por board
+  // mientras que la función para actualizar el tablero se llamará setBoard
+
+  //Estado para los turnos
+  const [turn, setTurn] = useState(TURNS.X); //El juego lo empieza las X
+
+  /*Encargado de todo lo que pasa en el juego
+    -De ver si hay ganador
+    -De actualizar el tablero
+    -Ver de quién es el turno
+  */
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X; // para que se actualice de quién es el turno
+    setTurn(newTurn); // se llama al ESTADO para que cambie el vlaor inicial
+    console.log(turn);
+  };
 
   return (
     <main className="board">
       <h1>Juego del gato</h1>
       <section className="game">
+        {/*updateBoard se pasa así para que sólo se active cuando se haga click */}
         {board.map((_, index) => {
           return (
-            <Square
-              key={index} //identificador único que se renderiza de una lista
-              //CON LA EXTENSIÓN DE REACT EN CHROME SE PUEDEN VER LOS COMPONENTES CON SU KEY
-              index={index} // para pasar por parámetro a Square
-            ></Square>
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
           );
         })}
+      </section>
+
+      {/*Sección para mostrar gráficamente de quién es el turno USANSO EL ESTADO
+      
+        Propósito de REACT: cambiar visualmente un componente a través del estado de el componente padre
+      */}
+      <section className="turn">
+        <h2>Turno</h2>
+        <Square>{turn === TURNS.X ? TURNS.X : TURNS.O}</Square>
       </section>
     </main>
   );
